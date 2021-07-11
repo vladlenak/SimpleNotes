182604247ade4af80a4f3b979051af6c2e51d61e
Vladlen <vladlenahtemov@gmail.com>
2021-07-11 14:31:25 +0300

    Написал DatabaseManager.kt и Note.kt

---
+++ app/src/main/java/akhtemov/vladlen/simplenotes/Note.kt
@@ -0,0 +1,7 @@
+package akhtemov.vladlen.simplenotes
+
+object Note {
+    var title = "Новая заметка"
+    var description = ""
+    var date = ""
+}
---
+++ app/src/main/java/akhtemov/vladlen/simplenotes/database/DatabaseManager.kt
@@ -0,0 +1,55 @@
+package akhtemov.vladlen.simplenotes.database
+
+import akhtemov.vladlen.simplenotes.Note
+import android.content.ContentValues
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+class DatabaseManager(context: Context) {
+    private val databaseHelper = DatabaseHelper(context)
+    var sqLiteDatabase: SQLiteDatabase? = null
+
+    fun openDb() {
+        sqLiteDatabase = databaseHelper.writableDatabase
+    }
+
+    fun insertToDb(title: String, description: String, date: String) {
+        val values = ContentValues().apply {
+            put(DatabaseConstant.TITLE_COLUMN, title)
+            put(DatabaseConstant.DESCRIPTION_COLUMN, description)
+            put(DatabaseConstant.DATE_COLUMN, date)
+        }
+
+        sqLiteDatabase?.insert(DatabaseConstant.NOTES_TABLE, null, values)
+    }
+
+    fun readDbData(): ArrayList<Note> {
+        val notesList = ArrayList<Note>()
+        val cursor = sqLiteDatabase?.query(
+            DatabaseConstant.NOTES_TABLE, null, null,
+            null, null, null, null
+        )
+
+        while (cursor?.moveToNext()!!) {
+            val dataTitle = cursor.getString(cursor.getColumnIndex(DatabaseConstant.TITLE_COLUMN))
+            val dataDescription =
+                cursor.getString(cursor.getColumnIndex(DatabaseConstant.DESCRIPTION_COLUMN))
+            val dataDate =
+                cursor.getString(cursor.getColumnIndex(DatabaseConstant.DATE_COLUMN))
+
+            val note = Note
+            note.title = dataTitle
+            note.description = dataDescription
+            note.date = dataDate
+
+            notesList.add(note)
+        }
+        cursor.close()
+
+        return notesList
+    }
+
+    fun closeDb() {
+        databaseHelper.close()
+    }
+}
