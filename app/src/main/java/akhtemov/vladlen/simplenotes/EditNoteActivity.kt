fb464dd92e7b536fc6af2c00108b989afa26ca47
Vladlen <vladlenahtemov@gmail.com>
2021-07-11 19:05:05 +0300

    Кастомный адаптер

---
+++ app/src/main/java/akhtemov/vladlen/simplenotes/EditNoteActivity.kt
@@ -0,0 +1,11 @@
+package akhtemov.vladlen.simplenotes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_note)
+    }
+}
---
+++ app/src/main/java/akhtemov/vladlen/simplenotes/IntentConstant.kt
@@ -0,0 +1,7 @@
+package akhtemov.vladlen.simplenotes
+
+object IntentConstant {
+    const val TITLE_KEY = "title_key"
+    const val DESCRIPTION_KEY = "description_key"
+    const val DATE_KEY = "date_key"
+}
