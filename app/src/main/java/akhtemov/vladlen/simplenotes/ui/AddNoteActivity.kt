f394f665acc1601f7a27c214093b5b5e706a8f49
Vladlen <vladlenahtemov@gmail.com>
2021-11-25 20:06:22 +0200

    Добавил Date Pickers. Заменил цвет иконки.

--- app/src/main/java/akhtemov/vladlen/simplenotes/ui/AddNoteActivity.kt
+++ app/src/main/java/akhtemov/vladlen/simplenotes/ui/AddNoteActivity.kt
@@ -4,10 +4,16 @@ import akhtemov.vladlen.simplenotes.R
 import akhtemov.vladlen.simplenotes.databinding.ActivityAddNoteBinding
 import android.app.Activity
 import android.content.Intent
+import android.icu.util.Calendar
+import android.icu.util.TimeZone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.util.Log
 import android.widget.Toast
+import com.google.android.material.datepicker.CalendarConstraints
+import com.google.android.material.datepicker.DateValidatorPointForward
+import com.google.android.material.datepicker.MaterialDatePicker
 
 class AddNoteActivity : AppCompatActivity() {
 
@@ -28,6 +34,24 @@ class AddNoteActivity : AppCompatActivity() {
                 else -> false
             }
         }
+
+        binding.datePickerTextView.setOnClickListener {
+            // A date range picker can be instantiated with
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                    .setTitleText(R.string.add_due_date)
+                // Opens the date picker with today's date selected.
+                    .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+                    .build()
+
+            // To show the picker to the user:
+            datePicker.show(supportFragmentManager, DATE_PICKER_TAG)
+
+            datePicker.addOnPositiveButtonClickListener {
+                val calendar = Calendar.getInstance(TimeZone.getTimeZone("UTC"))
+                calendar.timeInMillis = it
+                binding.datePickerTextView.text = calendar.time.toString()
+            }
+        }
     }
 
     private fun onClickSaveNoteButton() {
@@ -38,7 +62,7 @@ class AddNoteActivity : AppCompatActivity() {
         } else {
             val title = binding.titleTextField.text.toString()
             val desc = binding.descriptionTextField.text.toString()
-            val deadline = binding.deadlineEditText.text.toString()
+            val deadline = binding.datePickerTextView.text.toString()
 
             replyIntent.putExtra(TITLE_EXTRA_REPLY, title)
             replyIntent.putExtra(DESCRIPTION_EXTRA_REPLY, desc)
@@ -50,8 +74,12 @@ class AddNoteActivity : AppCompatActivity() {
     }
 
     companion object {
+        const val TAG = "AddNoteActivity"
+
         const val TITLE_EXTRA_REPLY = "title_extra_reply"
         const val DESCRIPTION_EXTRA_REPLY = "description_extra_reply"
         const val DEADLINE_EXTRA_REPLY = "deadline_extra_reply"
+
+        const val DATE_PICKER_TAG = "date_picker_tag"
     }
 }
