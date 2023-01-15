1b1a2837ce69d07b38a0e96f0a5203cffb7986db
Vladlen <vladlenahtemov@gmail.com>
2023-01-16 00:27:29 +0100

    Added Context and View extensions.

---
+++ app/src/main/java/akhtemov/vladlen/simplenotes/extension/ViewExt.kt
@@ -0,0 +1,15 @@
+package akhtemov.vladlen.simplenotes.extension
+
+import android.view.View
+
+fun View.gone(){
+    visibility = View.GONE
+}
+
+fun View.visible(){
+    visibility = View.VISIBLE
+}
+
+fun View.invisible(){
+    visibility = View.INVISIBLE
+}
